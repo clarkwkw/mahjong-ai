e19a65b08e199ed43fa757230281d36c65ad8eef
clarkwkw <clarkwkw@yahoo.com.hk>
2017-09-16 09:31:42 +0800

    add: Scoring_rules skeleton

---
+++ Scoring_rules/HK_rules.py
@@ -0,0 +1,44 @@
+def calculate_total_score(fixed_hand, hand, additional_tile, additional_tile_src, game):
+	pass
+
+def validate_hand(fixed_hand, hand, additional_tile):
+	pass
+
+def score_all_chows(fixed_hand, grouped_hand):
+	pass
+
+def score_honor_tiles(fixed_hand, grouped_hand):
+	pass
+
+def score_ones_nines(fixed_hand, grouped_hand):
+	pass
+
+def score_stolen_last_tile(additional_tile_src, game):
+	pass
+
+def score_mixed_suit(fixed_hand, grouped_hand):
+	pass
+
+def score_all_pongs(fixed_hand, grouped_hand):
+	pass
+
+def score_three_honors(fixed_hand, grouped_hand):
+	pass
+
+def score_pure_non_honor_suit(fixed_hand, grouped_hand):
+	pass
+
+def score_four_winds(fixed_hand, grouped_hand):
+	pass
+
+def score_pure_honor_suit(fixed_hand, grouped_hand):
+	pass
+
+def score_pure_ones_nines(fixed_hand, grouped_hand):
+	pass
+
+def score_all_pongs_with_steal(fixed_hand, grouped_hand, additional_tile_src):
+	pass
+
+def score_pure_one_to_nine(fixed_hand, grouped_hand, additional_tile_src):
+	pass
---
+++ Scoring_rules/__init__.py
@@ -0,0 +1,1 @@
+from . import HK_rules
