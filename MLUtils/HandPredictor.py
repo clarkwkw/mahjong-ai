848e855cea712dadf7ec1d1137b99fca9b5edbbe
clarkwkw <clarkwkw@yahoo.com.hk>
2018-01-10 12:58:55 +0800

    change: network settings

--- MLUtils/HandPredictor.py
+++ MLUtils/HandPredictor.py
@@ -19,14 +19,18 @@ class HandPredictor(AbstractDNN):
 				self.__X = tf.placeholder(tf.float32, [None, 2, 34, 1], name = "X")
 				self.__y_truth = tf.placeholder(tf.float32, [None, 34], name = "y_truth")
 
-				filter_chow = tf.get_variable("filter_chow", initializer = tf.random_normal([2, 3, 1, 1]))
-				bias_chow = tf.get_variable("bias_chow", initializer = tf.random_normal([1]))
+				filter_chow_1 = tf.get_variable("filter_chow_1", initializer = tf.random_normal([2, 3, 1, 1]))
+				bias_chow_1 = tf.get_variable("bias_chow_1", initializer = tf.random_normal([1]))
+				filter_chow_2 = tf.get_variable("filter_chow_2", initializer = tf.random_normal([1, 3, 1, 1]))
+				bias_chow_2 = tf.get_variable("bias_chow_2", initializer = tf.random_normal([1]))
+				
 				filter_pong = tf.get_variable("filter_pong", initializer = tf.random_normal([2, 1, 1, 1]))
 				bias_pong = tf.get_variable("bias_pong", initializer = tf.random_normal([1]))
 
-				conv_chow = tf.nn.relu(tf.nn.conv2d(self.__X, filter_chow, strides = [1, 1, 1, 1], padding = 'VALID') + bias_chow)
-				conv_chow = tf.pad(conv_chow, [[0, 0], [0, 0], [0, 2], [0, 0]])
-				
+				conv_chow = tf.nn.relu(tf.nn.conv2d(self.__X[:, :, 0:27, :], filter_chow_1, strides = [1, 1, 1, 1], padding = 'VALID') + bias_chow_1)
+				conv_chow = tf.pad(conv_chow, [[0, 0], [0, 0], [0,9], [0, 0]])
+				conv_chow = tf.nn.relu(tf.nn.conv2d(conv_chow, filter_chow_2, strides = [1, 1, 1, 1], padding = 'SAME') + bias_chow_2)
+
 				conv_pong = tf.nn.relu(tf.nn.conv2d(self.__X, filter_pong, strides = [1, 1, 1, 1], padding = 'VALID') + bias_pong)
 
 				combined = tf.concat([conv_chow, conv_pong], axis = 1)				
