6d4739bae0d3b69bca1653af5577a79770fe50cc
clarkwkw <clarkwkw@yahoo.com.hk>
2018-03-11 07:56:15 +0800

    update: ModelTrainer reward scheme

--- MoveGenerator/ModelTrainer.py
+++ MoveGenerator/ModelTrainer.py
@@ -4,11 +4,8 @@ from . import utils
 import numpy as np
 import Tile
 
-REWARD_VICTORY = 10
-REWARD_DRAW = 10
-REWARD_LOSE = 10
-REWARD_INVALID_DECISION = -1
-REWARD_NON_TERMINAL = 10
+REWARD_SAME = 100
+REWARD_DIFF = -100
 
 n_decisions = 42
 decisions_ = ["dots_chow", "dots_pong", "characters_chow", "characters_pong", "bamboo_chow", "bamboo_pong", "honor_pong", "no_action"]
@@ -19,6 +16,7 @@ class ModelTrainer(MoveGenerator):
 		self.__model = model(player_name = player_name, is_train = True, **kwargs)
 		self.__hmodel = RuleBasedAINaive("test", display_step = False)
 		self.__is_train = True
+		self.__pending_reward = 0
 
 	@property 
 	def is_train(self):
@@ -40,58 +38,68 @@ class ModelTrainer(MoveGenerator):
 
 	def decide_chow(self, player, new_tile, choices, neighbors, game):
 		self.begin_decision()
-		is_chow, choice = None, None
+		
 		if self.__is_train:
 			state = utils.dnn_encode_state(player, neighbors)
 			if self.__model.history_waiting:
-				self.__model.update_transition(state, REWARD_NON_TERMINAL)
+				self.__model.update_transition(state, self.__pending_reward)
 
-			is_chow, choice = self.__hmodel.decide_chow(player, new_tile, choices, neighbors, game)
-			valid_actions = [34 + decisions_.index("%s_chow"%new_tile.suit), 34 + decisions_.index("no_action")]
-			action_filter = np.zeros(n_decisions)
-			action_filter[valid_actions] = 1
-			action = 34 + decisions_.index("no_action") if not is_chow else 34 + decisions_.index("%s_chow"%new_tile.suit)
-			self.__model.update_history(state, action, action_filter)
+		h_is_chow, h_choice = self.__hmodel.decide_chow(player, new_tile, choices, neighbors, game)
+		valid_actions = [34 + decisions_.index("%s_chow"%new_tile.suit), 34 + decisions_.index("no_action")]
+		action_filter = np.zeros(n_decisions)
+		action_filter[valid_actions] = 1
+		action = 34 + decisions_.index("no_action") if not is_chow else 34 + decisions_.index("%s_chow"%new_tile.suit)
+		self.__model.update_history(state, action, action_filter)
+
+		is_chow, choice = self.__model.decide_chow(player, new_tile, choices, neighbors, game)
+		if is_chow == h_is_chow:
+			self.__pending_reward = REWARD_SAME
 		else:
-			is_chow, choice = self.__model.decide_chow(player, new_tile, choices, neighbors, game)
+			self.__pending_reward = REWARD_DIFF
 		self.end_decision()
 		return is_chow, choice
 
 	def decide_kong(self, player, new_tile, kong_tile, location, src, neighbors, game):
-		is_kong = None
 		self.begin_decision()
-		if self.__is_train:
-			state = utils.dnn_encode_state(player, neighbors)
-			if self.__model.history_waiting:
-				self.__model.update_transition(state, REWARD_NON_TERMINAL)
+		
+		state = utils.dnn_encode_state(player, neighbors)
+		if self.__model.history_waiting:
+			self.__model.update_transition(state, self.__pending_reward)
 
-			is_kong = self.__hmodel.decide_kong(player, new_tile, kong_tile, location, src, neighbors, game)
-			valid_actions = [34 + decisions_.index("%s_pong"%new_tile.suit), 34 + decisions_.index("no_action")]
-			action_filter = np.zeros(n_decisions)
-			action_filter[valid_actions] = 1
-			action = 34 + decisions_.index("no_action") if not is_kong else 34 + decisions_.index("%s_pong"%new_tile.suit)
-			self.__model.update_history(state, action, action_filter)
+		h_is_kong = self.__hmodel.decide_kong(player, new_tile, kong_tile, location, src, neighbors, game)
+		valid_actions = [34 + decisions_.index("%s_pong"%new_tile.suit), 34 + decisions_.index("no_action")]
+		action_filter = np.zeros(n_decisions)
+		action_filter[valid_actions] = 1
+		action = 34 + decisions_.index("no_action") if not is_kong else 34 + decisions_.index("%s_pong"%new_tile.suit)
+		self.__model.update_history(state, action, action_filter)
+
+		is_kong = self.__model.decide_kong(player, new_tile, kong_tile, location, src, neighbors, game)
+		if is_kong == h_is_kong:
+			self.__pending_reward = REWARD_SAME
 		else:
-			is_kong = self.__model.decide_kong(player, new_tile, kong_tile, location, src, neighbors, game)
+			self.__pending_reward = REWARD_DIFF
 		self.end_decision()
 		return is_kong
 
 	def decide_pong(self, player, new_tile, neighbors, game):
-		is_pong = None
 		self.begin_decision()
-		if self.__is_train:
-			state = utils.dnn_encode_state(player, neighbors)
-			if self.__model.history_waiting:
-				self.__model.update_transition(state, REWARD_NON_TERMINAL)
+		state = utils.dnn_encode_state(player, neighbors)
+		if self.__model.history_waiting:
+			self.__model.update_transition(state, self.__pending_reward)
 
-			is_pong = self.__hmodel.decide_pong(player, new_tile, neighbors, game)
-			valid_actions = [34 + decisions_.index("%s_pong"%new_tile.suit), 34 + decisions_.index("no_action")]
-			action_filter = np.zeros(n_decisions)
-			action_filter[valid_actions] = 1
-			action = 34 + decisions_.index("no_action") if not is_pong else 34 + decisions_.index("%s_pong"%new_tile.suit)
-			self.__model.update_history(state, action, action_filter)
+		h_is_pong = self.__hmodel.decide_pong(player, new_tile, neighbors, game)
+		valid_actions = [34 + decisions_.index("%s_pong"%new_tile.suit), 34 + decisions_.index("no_action")]
+		action_filter = np.zeros(n_decisions)
+		action_filter[valid_actions] = 1
+		action = 34 + decisions_.index("no_action") if not is_pong else 34 + decisions_.index("%s_pong"%new_tile.suit)
+		self.__model.update_history(state, action, action_filter)
+	
+		is_pong = self.__model.decide_pong(player, new_tile, neighbors, game)
+		if h_is_pong == is_pong:
+			self.__pending_reward = REWARD_SAME
 		else:
-			is_pong = self.__model.decide_pong(player, new_tile, neighbors, game)
+			self.__pending_reward = REWARD_DIFF
+
 		self.end_decision()
 		return is_pong
 
@@ -99,29 +107,34 @@ class ModelTrainer(MoveGenerator):
 		self.begin_decision()
 		if self.__is_train and self.__model.history_waiting:
 				state = utils.dnn_encode_state(player, neighbors)
-				self.__model.update_transition(state, REWARD_VICTORY)
+				self.__model.update_transition(state, self.__pending_reward)
 		self.end_decision()
 		return True
 
 	def decide_drop_tile(self, player, new_tile, neighbors, game):
-		drop_tile = None
 		self.begin_decision()
-		if self.__is_train:
-			state = utils.dnn_encode_state(player, neighbors)
-			if self.__model.history_waiting:
-				self.__model.update_transition(state, REWARD_NON_TERMINAL)
-			valid_actions = []
-			tiles = player.hand if new_tile is None else player.hand + [new_tile]
-			for tile in tiles:
-				valid_actions.append(Tile.convert_tile_index(tile))
+		
+		state = utils.dnn_encode_state(player, neighbors)
+		if self.__model.history_waiting:
+			self.__model.update_transition(state, self.__pending_reward)
+		valid_actions = []
+		tiles = player.hand if new_tile is None else player.hand + [new_tile]
+		for tile in tiles:
+			valid_actions.append(Tile.convert_tile_index(tile))
 
-			action_filter = np.zeros(n_decisions)
-			action_filter[valid_actions] = 1
+		action_filter = np.zeros(n_decisions)
+		action_filter[valid_actions] = 1
 
-			drop_tile = self.__hmodel.decide_drop_tile(player, new_tile, neighbors, game)
-			action = Tile.convert_tile_index(drop_tile)
-			self.__model.update_history(state, action, action_filter)
+		h_drop_tile = self.__hmodel.decide_drop_tile(player, new_tile, neighbors, game)
+		action = Tile.convert_tile_index(drop_tile)
+		self.__model.update_history(state, action, action_filter)
+
+		drop_tile = self.__model.decide_drop_tile(player, new_tile, neighbors, game)
+
+		if drop_tile == h_drop_tile:
+			self.__pending_reward = REWARD_SAME
 		else:
-			drop_tile = self.__model.decide_drop_tile(player, new_tile, neighbors, game)
+			self.__pending_reward = REWARD_DIFF
+
 		self.end_decision()
 		return drop_tile
